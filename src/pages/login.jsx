bc99c4e806086bdc7bb7901b86ed15f1f69d8f63
Affi-Amine <130100363+Affi-Amine@users.noreply.github.com>
2024-05-25 23:06:35 +0000

    fixing login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -37,7 +37,7 @@ const LoginPage = () => {
                 className="text-sm font-medium text-gray-900 hover:underline dark:text-gray-50"
                 href="#"
               >
-                Forgot your password?
+                Forgot your password
               </a>
             </div>
             <Input id="password" required type="password" />
