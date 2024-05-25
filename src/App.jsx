fb293ea75f492d4f662fe12a7569e4696ae0da36
ElyesBelgouthi <belelyesbel@gmail.com>
2024-05-25 13:41:36 +0100

    adding the layout of signin

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import ChatPage from "./pages/ChatPage";
 import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/login";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <ProfilePage />,
+    element: <LoginPage />,
   },
   {
     path: "/chat",
