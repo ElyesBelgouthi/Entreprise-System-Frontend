a60f8e5dc862fccfe0b914a9892b02ebdd0f6c47
ElyesBelgouthi <belelyesbel@gmail.com>
2024-05-26 20:26:07 +0100

    fixing main header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,6 +13,7 @@ import {
   DropdownMenu,
 } from "../app/ui/dropdown-menu";
 import { AvatarImage, AvatarFallback, Avatar } from "../app/ui/avatar";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
@@ -24,28 +25,30 @@ const Header = () => {
         <NavigationMenu className="hidden lg:flex">
           <NavigationMenuList>
             <NavigationMenuLink asChild>
-              <a
+              <NavLink
+                to="/feed"
                 className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
                 href="#"
               >
                 Feed
-              </a>
+              </NavLink>
             </NavigationMenuLink>
             <NavigationMenuLink asChild>
-              <a
+              <NavLink
+                to="/chat"
                 className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
                 href="#"
               >
                 Chat
-              </a>
+              </NavLink>
             </NavigationMenuLink>
             <NavigationMenuLink asChild>
-              <a
+              <NavLink
                 className="group inline-flex h-9 w-max items-center justify-center rounded-md bg-white px-4 py-2 text-sm font-medium transition-colors hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-gray-100/50 data-[state=open]:bg-gray-100/50 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus:bg-gray-800 dark:focus:text-gray-50 dark:data-[active]:bg-gray-800/50 dark:data-[state=open]:bg-gray-800/50"
                 href="#"
               >
                 Notifications
-              </a>
+              </NavLink>
             </NavigationMenuLink>
           </NavigationMenuList>
         </NavigationMenu>
