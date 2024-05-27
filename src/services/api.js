4f0313dbd592ed1371fb0a730e5d4dd6a97d2001
samer-mansouri <samer.mansouri@insat.ucar.tn>
2024-05-27 04:29:35 +0100

    Adding token to header

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
+import store from "@/redux/store/store";
 
 const api = axios.create({
     baseURL: "http://localhost:3000",
+});
+
+//bind the token to the header
+api.interceptors.request.use((config) => {
+    const token = store.getState().mainReducer.userToken;
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
 });
 
 export default api;
